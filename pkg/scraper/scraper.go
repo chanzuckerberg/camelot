84a9fb2a3678211491f2b3359fd14a0d2e96f8f4
Alex Lokshin <alokshin@chanzuckerberg.com>
2023-09-12 15:45:44 -0400

    Added contextual details

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -7,14 +7,17 @@ import (
 	"github.com/chanzuckerberg/camelot/pkg/scraper/types"
 )
 
+// Depends on env.AWS_PROFILE
 func ScrapeAWS(profile string) (*types.InventoryReport, error) {
 	return aws.Scrape(profile)
 }
 
+// Depends on env.GITHUB_TOKEN
 func ScrapeGithub(githubOrg string) (*types.InventoryReport, error) {
 	return github.Scrape(githubOrg)
 }
 
+// Depends on env.TFE_TOKEN and env.TFE_ADDRESS
 func ScrapeTFC(orgName string) (*types.InventoryReport, error) {
 	return tfc.Scrape()
 }
